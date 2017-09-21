7a6eda3964d668ecd723de1a2d410f06ef3a1d15
Barney Carroll <barney.carroll@gmail.com>
2017-09-21 15:53:15 +0100

    Works in the browser

--- xet.js
+++ xet.js
@@ -1,6 +1,8 @@
-module.exports = function xet( map, key, factory ){
+function xet( map, key, factory ){
 	if( !map.has( key ) )
 		map.set( key, factory() )
 
 	return map.get( key )
 }
+
+try {module.exports = xet} catch(e){}
